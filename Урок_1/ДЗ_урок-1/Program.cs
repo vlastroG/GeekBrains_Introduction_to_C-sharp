89823c710653ca578ea65ade64e659606a7aa4e9
Владимир Строганов <vlastroganov@gmail.com>
2021-11-09 19:27:04 +0300

    created first file

---
+++ Урок_1/ДЗ_урок-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ДЗ_урок_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
