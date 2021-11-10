e1fb71e7ceedaea5fe1e1ad7729dcde429ca000b
Vladimir <vlastroganov@gmail.com>
2021-11-10 23:58:54 +0300

    Easy cycle with love

---
+++ Урок_3/Project-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-нибудь интересное о своей жизни:)");
+            string phrase = Console.ReadLine();
+            for (int i = (phrase.Length - 1); i > -1; i--)
+            {
+                Console.Write(phrase[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
