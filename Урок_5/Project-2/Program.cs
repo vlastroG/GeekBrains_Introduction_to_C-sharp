71b34fd4e71a3b9719f500abd365ffa03bb7d9a6
Vladimir <vlastroganov@gmail.com>
2021-11-14 15:39:05 +0300

    Урок 5, задание 2.

---
+++ Урок_5/Project-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "startup.txt";
+            var time = DateTime.Now.ToString();
+            File.AppendAllLines(fileName, new[] { $"Программа была запущена {time}. Такие дела." });
+            Console.WriteLine("Содержимое файла \"startup.txt\":");
+            for (int i = 0; i < File.ReadAllLines(fileName).Length; i++)
+            {
+                Console.WriteLine(File.ReadAllLines(fileName)[i]); 
+            }
+        }
+    }
+}
